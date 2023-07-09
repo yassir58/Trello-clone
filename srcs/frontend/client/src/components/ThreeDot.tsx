ead8475fc8bb949c2b97a71617a2e57fe3dc5a9f
Ma3ert <yahyaitiazza15@gmail.com>
2023-07-09 16:40:44 +0100

    add the file for the threedot

---
+++ srcs/frontend/client/src/components/ThreeDot.tsx
@@ -0,0 +1,37 @@
+import { Button, Text, Stack, Divider } from "@chakra-ui/react";
+
+function ThreeDot() {
+  return (
+    <Stack
+      padding={3}
+      borderRadius="xl"
+      direction="column"
+      bg="white"
+      shadow="md"
+    >
+      <Button
+        py={8}
+        borderRadius="xl"
+        variant="ghost"
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
+      >
+        <Text>Rename</Text>
+      </Button>
+      <Divider/>
+      <Button
+        py={8}
+        borderRadius="xl"
+        variant="ghost"
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
+      >
+        <Text>Delete</Text>
+      </Button>
+    </Stack>
+  );
+}
+
+export default ThreeDot;
