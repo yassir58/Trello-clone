634722a604453edf252fd044d0e3a5a0561dae17
lhaj-matich <choumouloussama@gmail.com>
2023-08-01 19:38:27 +0100

    Rewrote the email handler.

--- srcs/backend/utils/Email.ts
+++ srcs/backend/utils/Email.ts
@@ -1,31 +1,50 @@
 import nodemailer from "nodemailer";
+import pug from "pug";
 
-interface emailOptions {
-  to: string;
-  from: string;
-  subject: string;
-  text: string;
+class Email {
+  private to;
+  private name;
+  private url;
+  private from;
+
+  constructor(user: any, url: string) {
+    this.to = user.email;
+    this.name = user.fullname.split(" ")[0];
+    this.url = url;
+    this.from = process.env.EMAIL_USER;
+  }
+
+  newTransport() {
+    return nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  async send(template: string, subject: string) {
+    const html = pug.renderFile(`${__dirname}/../views/emailTemplates/${template}.pug`, {
+      subject,
+      name: this.name,
+      url: this.url,
+    });
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendPasswordReset() {
+    await this.send("resetToken", "Your password reset token (valid for only 10 minutes)");
+  }
 }
 
-const sendMail = async (options: emailOptions) => {
-  const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 25,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const email: emailOptions = {
-    to: options.to,
-    from: options.from,
-    subject: options.subject,
-    text: options.text,
-  };
-
-  console.log(email);
-  transporter.sendMail(email);
-};
-
-export default sendMail;
+export default Email;
